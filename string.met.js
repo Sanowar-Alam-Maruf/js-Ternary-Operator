0546fc53f05fa4730679f2d7968c162a3319e3c3
unknown <Sanowar-Alam-Maruf>
2025-06-25 22:40:28 +0600

    first commit

---
+++ string.met.js
@@ -0,0 +1,46 @@
+// length
+let names='ia sanowar';
+let res=names.length;
+console.log(res);
+
+// toUpperCase
+let name02='sanowar alam maruf';
+let res02=name02.toUpperCase()
+console.log(res02);
+
+//toLowerCase
+let name03='SANOWAR ALAM MARUF';
+let res03=name03.toLowerCase()
+console.log(res03);
+
+
+//includes
+let text='The Ternary Operator in Java Script is a shortcut for writing simple if-else statements';
+let textHave=text.includes('Java')
+console.log(textHave);
+
+//indexOf
+let text02='Ternary Operator';
+let position=text02.indexOf('O')
+console.log(position);
+
+//slice
+let text03='Operator';
+let sli=text03.slice(1,4)
+console.log(sli);
+
+//replace
+let text04='shortcut for writing';
+let exchange=text04.replace('writing','reading')
+console.log(exchange);
+
+
+//trim
+let nam='salam   ';
+let sps=nam.trim()
+console.log(sps);
+
+//split
+let nams='raju,salam,manik,sofik';
+let newArray=nams.split(',')
+console.log(newArray);
